424f8d38c34c73c4b59ff9012973a9a768f2ce8c
Rhecus <122747158+Rhecus@users.noreply.github.com>
2023-01-19 22:24:41 +0100

    Update Ray.cpp

--- Sources/math/Ray.cpp
+++ Sources/math/Ray.cpp
@@ -1,5 +1,26 @@
-#include "math/Ray.hpp"
+//
+//  Ray.hpp
+//  Ray_tracing
+//
+//
 
+#ifndef Ray_hpp
+#define Ray_hpp
+#pragma once
+#include "Point.hpp"
+#include "Vec.hpp"
 namespace math {
-	
+    struct Ray {
+        
+        Point origin;
+        Vec direction;
+        
+        Ray(Point origin_,Vec direction_);
+        Ray(Point A,Point B);
+        
+        //Ray& reflected(const Intersertion& intersec);
+
+    };
 }
+
+#endif /* Ray_hpp */
