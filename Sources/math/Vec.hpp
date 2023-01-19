da9e360c3f912a2fb666fee1241557fb369e9184
Rhecus <122747158+Rhecus@users.noreply.github.com>
2023-01-19 22:19:26 +0100

    Update Vec.hpp

--- Sources/math/Vec.hpp
+++ Sources/math/Vec.hpp
@@ -1,7 +1,25 @@
+//
+//  Vec.hpp
+//  Ray_tracing
+
+
+#ifndef Vec_hpp
+#define Vec_hpp
 #pragma once
 
 namespace math {
-	struct Vec {
-		
-	};
+    struct Vec {
+        float x;
+        float y;
+        float z;
+        
+        float dot(const Vec& rhs) const;
+        float lenght_sq() const;
+        float lenght() const;
+        void normalize();
+        Vec normalized() const;
+        
+    };
 }
+
+#endif /* Vec_hpp */
