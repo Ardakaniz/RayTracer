6c81d1627dd50827b4f93fe752fc415a3f0293bb
Adrien Renaudineau <adrien.renaudineau@universite-paris-saclay.fr>
2023-01-21 22:03:49 +0100

    Impl Ray::reflected (with TODOs)

--- Sources/math/Ray.cpp
+++ Sources/math/Ray.cpp
@@ -4,23 +4,38 @@
 //
 //
 
-#include "Ray.hpp"
+#include "math/Ray.hpp"
+#include "math/Intersection.hpp"
+#include "Object.hpp"
 
-math::Ray::Ray(Point origin_, Vec direction_)
-{
-    origin=origin_;
-    direction=direction_;
+#include <cassert> // TMP
+
+namespace math {
+    Ray::Ray(Point origin_, Vec direction_) {
+        origin = origin_;
+        direction = direction_;
+    }
+
+    Ray::Ray(Point A, Point B) {
+        origin = A;
+        Vec dir;
+        dir.x=B.x-A.x;
+        dir.y=B.y-A.y;
+        dir.z=B.z-A.z;
+        dir.normalize();
+        direction=dir;
+    }
+
+    Ray Ray::reflected(const Intersection& intersec) const {
+        // TODO: Object::get_normal_at
+        // TODO: float * vec
+        
+        /* const Vec normal{ intersec.obj.get_normal_at(intersec.point) };
+        const Vec dir{ intersec.ray.direction - 2.f * normal.dot(intersec.ray.direction) * normal }; 
+        
+        return Ray{ intersec.point, dir }; */
+
+        assert(false && "TODO");
+        return intersec.ray;
+    }
 }
-
-math::Ray::Ray(Point A, Point B)
-{
-    origin=A;
-    Vec dir;
-    dir.x=B.x-A.x;
-    dir.y=B.y-A.y;
-    dir.z=B.z-A.z;
-    dir.normalize();
-    direction=dir;
-    
-}
-
--- Sources/math/Ray.hpp
+++ Sources/math/Ray.hpp
@@ -7,19 +7,21 @@
 #ifndef Ray_hpp
 #define Ray_hpp
 #pragma once
+
 #include "Point.hpp"
 #include "Vec.hpp"
+
 namespace math {
+    struct Intersection;
+
     struct Ray {
-        
         Point origin;
         Vec direction;
         
-        Ray(Point origin_,Vec direction_);
-        Ray(Point A,Point B);
+        Ray(Point origin_, Vec direction_);
+        Ray(Point A, Point B);
         
-        //Ray& reflected(const Intersertion& intersec);
-
+        Ray reflected(const Intersection& intersec) const;
     };
 }
 
