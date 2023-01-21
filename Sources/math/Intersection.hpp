c9294791f2e94ebc4d62efffdbdcae682ff6d6be
Adrien Renaudineau <adrien.renaudineau@universite-paris-saclay.fr>
2023-01-21 22:02:46 +0100

    Make math::Intersection

---
+++ Sources/math/Intersection.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "math/Point.hpp"
+
+class Object;
+class Ray;
+
+namespace math {
+    struct Intersection {
+        // Intersection point
+        Point point;
+
+        // Intersection between the `ray` and an `object`
+        Object& obj;
+        Ray& ray;
+    };
+}
