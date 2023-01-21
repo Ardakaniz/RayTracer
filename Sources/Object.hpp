86824a62b3910fbd2a0239304931066913c890c3
Rhecus <122747158+Rhecus@users.noreply.github.com>
2023-01-21 22:11:50 +0100

    Update Object.hpp

--- Sources/Object.hpp
+++ Sources/Object.hpp
@@ -1,5 +1,19 @@
+//
+//  Object.hpp
+//  Ray_tracing
+//
+//
+
+#ifndef Object_hpp
+#define Object_hpp
 #pragma once
+#include "Ray.hpp"
 
 class Object {
-
+    
+    std::optional<Intersection> intersection(const math::Ray& ray);
+    Vec normal(const Point& pt);
 };
+
+
+#endif /* Object_hpp */
