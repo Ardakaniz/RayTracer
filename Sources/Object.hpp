b96045e2112649e8344fb7bebba9005f2f052b69
Lucien M <lucienmarquant@gmail.com>
2023-01-23 19:28:48 +0100

    fix object.hpp

--- Sources/Object.hpp
+++ Sources/Object.hpp
@@ -7,12 +7,16 @@
 #ifndef Object_hpp
 #define Object_hpp
 #pragma once
-#include "Ray.hpp"
+
+#include "math/Ray.hpp"
+#include "math/Point.hpp"
+#include "math/Vec.hpp"
+#include <optional>
 
 class Object {
-    
+public:
     std::optional<Intersection> intersection(const math::Ray& ray);
-    Vec get_normal_at(const Point& pt);
+    Vec get_normal_at(const Point& pt);    
 };
 
 
