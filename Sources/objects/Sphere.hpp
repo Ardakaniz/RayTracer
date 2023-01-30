b4b3949cc3c2b7b15f0e657ee23045d07dd0fcb0
Lucien M <lucienmarquant@gmail.com>
2023-01-30 17:03:22 +0100

    Creation Sphere sauf avec norm

--- Sources/objects/Sphere.hpp
+++ Sources/objects/Sphere.hpp
@@ -2,6 +2,24 @@
 
 #include "Object.hpp"
 
-class Sphere : public Object {
 
+namespace math {
+
+class Sphere : public Object {
+    
+public:
+    Point Center;
+    float Radius;
+    
+    Sphere(Point Center, float Radius);
+    Sphere(Point Center, Point B);
+    
+    std::optional<Intersection> intersection(const Ray& ray) override;
+    
+    Vec get_normal_at(const math::Point& pt);
+    
 };
+
+}
+
+
