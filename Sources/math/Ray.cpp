4a6467994872f7350fc436c5852c57644479d22b
Adrien Renaudineau <adrien.renaudineau@universite-paris-saclay.fr>
2023-01-30 17:00:21 +0100

    Impl Ray::reflected

--- Sources/math/Ray.cpp
+++ Sources/math/Ray.cpp
@@ -1,14 +1,6 @@
-//
-//  Ray.cpp
-//  Ray_tracing
-//
-//
-
 #include "math/Ray.hpp"
 #include "math/Intersection.hpp"
 #include "Object.hpp"
-
-#include <cassert> // TMP
 
 namespace math {
     Ray::Ray(Point origin_, Vec direction_) {
@@ -26,16 +18,10 @@ namespace math {
         direction=dir;
     }
 
-    Ray Ray::reflected(const Intersection& intersec) const {
-        // TODO: Object::get_normal_at
-        // TODO: float * vec
+    Ray Ray::reflected(const Intersection& intersec) const {        
+        const Vec normal{ intersec.obj.get_normal_at(intersec.point) };
+        const Vec dir = Vec{ intersec.ray.direction - 2.f * normal.dot(intersec.ray.direction) * normal }.normalized(); 
         
-        /* const Vec normal{ intersec.obj.get_normal_at(intersec.point) };
-        const Vec dir{ intersec.ray.direction - 2.f * normal.dot(intersec.ray.direction) * normal }; 
-        
-        return Ray{ intersec.point, dir }; */
-
-        assert(false && "TODO");
-        return intersec.ray;
+        return Ray{ intersec.point, dir };
     }
 }
