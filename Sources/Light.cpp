0d7307a95167fb1aef22ff67d054d394166d4ffd
Adrien Renaudineau <adrien.renaudineau@universite-paris-saclay.fr>
2023-02-06 16:12:38 +0100

    Light formatting

--- Sources/Light.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  Light.cpp
-//  Ray_tracingunoff
-//
-//  Created by lucien marquant on 30/01/2023.
-//
-
-#include "Light.hpp"
--- Sources/Light.hpp
+++ Sources/Light.hpp
@@ -1,12 +1,9 @@
 #pragma once
 
 #include "math/Color.hpp"
-#include "math/Ray.hpp"
 #include "math/Point.hpp"
-#include "math/Vec.hpp"
+
 struct Light {
-    
     math::Point pos;
     math::Color color;
-    
 };
