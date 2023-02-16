1eadfcfcc55c0bbfdafcf0a0f0b3b9ecc59e38c6
Lucien M <lucienmarquant@gmail.com>
2023-02-16 20:21:37 +0100

    Hearth constructor,  get_normal_at and fix d^3-->d

--- Sources/objects/Hearth.cpp
+++ Sources/objects/Hearth.cpp
@@ -1,10 +1,18 @@
 
 
-#include "Hearth.hpp"
+#include "objects/Hearth.hpp"
+
+Hearth::Hearth(math::Point pos, float a_,float b_, float c_, float d_,const math::Color& diffuse_color) :
+Object(pos,diffuse_color),
+a(a_),
+b(b_),
+c(c_),
+d(d_)
+{}
 
 float Hearth::f(math::Point M)
 {
-    float left=(M.x-_pos.x)*(M.x-_pos.x)+a*(M.y-_pos.y)*(M.y-_pos.y)+(M.z-_pos.z)*(M.z-_pos.z)-d*d*d;
+    float left=(M.x-_pos.x)*(M.x-_pos.x)+a*(M.y-_pos.y)*(M.y-_pos.y)+(M.z-_pos.z)*(M.z-_pos.z)-d;
     float right=(M.z-_pos.z)*(M.z-_pos.z)*(M.z-_pos.z)*(c*(M.x-_pos.x)*(M.x-_pos.x)+b*(M.y-_pos.y)*(M.y-_pos.y));
     return left*left*left-right;
 }
@@ -19,7 +27,7 @@ float Hearth::f_tp(const math::Ray &ray, float t)
     float g_x=(ray.origin.x-_pos.x+t*ray.direction.x); //left part of parametric eq ->x
     float g_y=(ray.origin.y-_pos.y+t*ray.direction.y); //left part of parametric eq ->y
     float g_z=(ray.origin.z-_pos.z+t*ray.direction.z); //left part of parametric eq ->z
-    float g=g_x*g_x+a*g_y*g_y+g_z*g_z-d*d*d; // left part without the cube power
+    float g=g_x*g_x+a*g_y*g_y+g_z*g_z-d; // left part without the cube power
     float gp=2*ray.direction.x*g_x+a*2*ray.direction.y*g_y+2*ray.direction.z*g_z; //derivate respect to t
     float left_derivative=gp*( 3*g*g);
     
@@ -33,4 +41,22 @@ float Hearth::f_tp(const math::Ray &ray, float t)
     return left_derivative+right_derivative;
 }
 
+std::optional<math::Intersection> Hearth::intersection(const math::Ray& ray) const
+{
+    return std::nullopt;
+}
+
+math::Vec Hearth::get_normal_at(const math::Point& pt) const
+{
+    //we will compute here grad(f) at pt
+    float g=(pt.x-_pos.x)*(pt.x-_pos.x)+a*(pt.y-_pos.y)*(pt.y-_pos.y)+(pt.z-_pos.z)*(pt.z-_pos.z)-d; // left part (under the cubic root)
+    float g_z3=(pt.z-_pos.z)*(pt.z-_pos.z)*(pt.z-_pos.z);
+    float df_x=3*2*(pt.x-_pos.x)*g*g-g_z3*c*2*(pt.x-_pos.x);
+    float df_y=3*2*a*(pt.y-_pos.y)*g*g-g_z3*b*2*(pt.y-_pos.y);
+    float df_z=3*2*(pt.z-_pos.z)*g*g-3*(pt.z-_pos.z)*(pt.z-_pos.z)*( c*(pt.x-_pos.x)*(pt.x-_pos.x)+b*(pt.y-_pos.y)*(pt.y-_pos.y) );
+    math::Vec n={df_x,df_y,df_z};
+    n.normalize();
+    return n;
+    
+}
 
--- Sources/objects/Hearth.hpp
+++ Sources/objects/Hearth.hpp
@@ -3,9 +3,9 @@
 #define Hearth_hpp
 
 #include "Object.hpp"
-#include "Point.hpp"
-#include "Ray.hpp"
-#include "Vec.hpp"
+#include "math/Point.hpp"
+#include "math/Ray.hpp"
+#include "math/Vec.hpp"
 
 class Hearth : public Object {
     
@@ -16,15 +16,18 @@ public:
     // b/c have almost the same role, the bigest one give the orientation by its sign.  positive imply up (ie the peak is botside)
     float d; // d>0, overall size
     // (x^2+a^2+z^2-1)^3-c.x^2.z^3-b.y^2.z^3=0 parametric equation
-    float Gamma(math::Point A,math::Vec u);
+    
+    
+    Hearth(math::Point pos, float a_,float b_, float c_, float d_,const math::Color& diffuse_color = math::WHITE);
     
     float f(math::Point M); // parametric equation
     float f_t(const math::Ray &ray ,float t); //  f_t=0 equation to solve to find intersection
     float f_tp(const math::Ray &ray,float t); // its derivative to compute zero with Newton algorithm
     
-    Hearth(math::Point pos, float a_,float b_, float c_, float d_,const math::Color& diffuse_color = math::WHITE);
     
-    std::optional<math::Intersection> intersection(const math::Ray& ray) const override;
+    
+    std::optional<math::Intersection> intersection(const math::Ray& ray) const override ;
     
     math::Vec get_normal_at(const math::Point& pt) const override;
+};
 #endif /* Hearth_hpp */
