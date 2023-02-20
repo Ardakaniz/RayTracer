7009cf96f3ade7c587961fd933ae0949eebffd87
Lucien M <lucienmarquant@gmail.com>
2023-02-20 20:58:24 +0100

    Box, Works for a cube, need more investigation for over shape of boxes

--- Sources/objects/Box.cpp
+++ Sources/objects/Box.cpp
@@ -1,1 +1,40 @@
 #include "objects/Box.hpp"
+#include "objects/utils.hpp"
+
+Box::Box(const math::Point& pos,float a_,float b_, float c_,float R_b_,const math::Color& diffuse_color) :
+Object(pos, diffuse_color),
+a(a_),
+b(b_),
+c(c_),
+R_b(R_b_),
+_sphere(pos, 3.f)
+{}
+
+std::optional<math::Intersection> Box::intersection(const math::Ray& ray) const {
+    return utils::compute_intersec(
+        _sphere,
+        *this,
+        ray,
+        [&](const math::Ray& ray, float t) {
+            return f(ray.origin.translated(t*ray.direction));
+        }
+    );
+}
+math::Vec Box::get_normal_at(const math::Point &pt) const
+{
+    //we will compute here grad(f) at pt
+    const float df_x=6*pt.x*pt.x*pt.x*pt.x*pt.x/(a*a*a*a*a*a);
+    const float df_y=6*pt.y*pt.y*pt.y*pt.y*pt.y/(b*b*b*b*b*b);
+    const float df_z=6*pt.z*pt.z*pt.z*pt.z*pt.z/(c*c*c*c*c*c);
+    math::Vec n={df_x,df_y,df_z};
+    n.normalize();
+    return n;
+}
+
+float Box::f(const math::Point &M) const
+{
+    float X6=(M.x-_pos.x)*(M.x-_pos.x)*(M.x-_pos.x)*(M.x-_pos.x)*(M.x-_pos.x)*(M.x-_pos.x)/(a*a*a*a*a*a);
+    float Y6=(M.y-_pos.y)*(M.y-_pos.y)*(M.y-_pos.y)*(M.y-_pos.y)*(M.y-_pos.y)*(M.y-_pos.y)/(b*b*b*b*b*b);
+    float Z6=(M.z-_pos.z)*(M.z-_pos.z)*(M.z-_pos.z)*(M.z-_pos.z)*(M.z-_pos.z)*(M.z-_pos.z)/(c*c*c*c*c*c);
+    return X6+Y6+Z6-R_b;
+}
--- Sources/objects/Box.hpp
+++ Sources/objects/Box.hpp
@@ -1,7 +1,31 @@
 #pragma once
 
 #include "Object.hpp"
+#include "math/Point.hpp"
+#include "math/Ray.hpp"
+#include "math/Vec.hpp"
 
+#include "objects/Sphere.hpp"
 class Box : public Object {
-
+    public:
+    float a;
+    float b;
+    float c;
+    float R_b;
+    
+    // (x/a)^6+(y/b)^6+(z/c)^6=R_b
+    
+    Box(const math::Point& pos,float a_,float b_, float c_,float R_b_,const math::Color& diffuse_color= math::WHITE);
+    
+    std::optional<math::Intersection> intersection(const math::Ray& ray) const override ;
+    
+    math::Vec get_normal_at(const math::Point& pt) const override;
+    
+private:
+    float f(const math::Point& M) const; // parametric equation
+    Sphere _sphere;
+    
+    
+    
 };
+
